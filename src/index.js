2a5b181703a391f74afb7345f31650c5c1c407b4
huonchan <dmht1127@gmail.com>
2020-07-08 22:34:05 +0900

    イベント処理の確認＋動画変更

--- src/index.js
+++ src/index.js
@@ -7,16 +7,48 @@ import YouTube from 'react-youtube';
 
 //todo:別ファイルに分けろ。あと選択肢は外部ファイルか何かに定義しろ。
 class Menu extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Music List'
+    }
+  }
+
+  handleClick = () => {
+    console.log('this is:', this);
+    this.state.title = 'Change Title'
+
+    this.render()
+  }
+
   render() {
+
+    const title = this.state.title;
+
     return (
+
       <div id ='Menu' class='container_items'>
-        <p>Menu Component</p>
+        <p>Menu {title}</p>
+        <input
+          type="text"
+          value={title}
+          onChange={ e  => {
+            this.setState({
+              title: e.target.value
+            })
+          }}
+        />
         <p>バッハの旋律を夜に聴いたせいです</p>
         <p>G線状のアリア</p>
         <p>ぱわぱふOP</p>
         <p>Powerpuff Raves</p>
         <p>ムーンライト</p>
         <p>マイム</p>
+
+        <button onclick={this.handleClick}>
+          イベントボタン
+        </button>
 
       </div>
     )
@@ -36,7 +68,7 @@ class Youtube extends React.Component {
     return (
       <div id ='movie' class='container_items'>
         <YouTube
-          videoId='KSC94cDM85s'
+          videoId='DFJgvgmroJs'
           opts={opts}
           onReady={this._onReady}
         />
